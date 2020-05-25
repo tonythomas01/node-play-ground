6984aea21f7deecbc5a7e6bd5e61d852ee6ef57b
Tony Thomas <tony.thomas@keyflow.se>
2020-05-25 12:24:18 +0200

    Intiial stuff, base express, linters

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const getYear = (date: Date): string => {
+  return `${date.getFullYear()}`;
+};
+app.get('/', (req, res) => {
+  const year = getYear(new Date());
+  res.send(`Hello World ! ${year}`);
+});
+app.listen(3000, () => {
+  // tslint:disable-next-line:no-console
+  console.log('App listening on port 3000!');
+});
